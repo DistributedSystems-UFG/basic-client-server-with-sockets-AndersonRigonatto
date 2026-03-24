37186668cf79a117ab45a4cecba019c1dcb2b3b5
Anderson Rigonatto <andersonrigonatto@gmail.com>
2026-03-23 22:45:53 -0300

    feat: cliente interativo com loop de requisicoes

--- client.py
+++ client.py
@@ -1,9 +1,23 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
-s.close()               # close the connection
+s.connect((HOST, PORT))
+
+print("Conectado ao servidor.")
+print("Operacoes disponiveis: upper, lower, alternate")
+print("Formato: operacao:texto")
+print("Digite 'exit' para sair.\n")
+
+while True:
+    message = input(">> ")
+
+    if message.strip().lower() == 'exit':
+        break
+
+    s.send(message.encode())
+    data = s.recv(1024)
+    print("Resposta: " + data.decode() + "\n")
+
+s.close()
+print("Conexao encerrada.")
